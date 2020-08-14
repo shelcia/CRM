e3d5ede4c5a2d6e99af4d75b4cee9b499de60816
noob <shelcia1999@gmail.com>
2020-08-14 11:50:17 +0530

    update edit service

--- src/components/admin/ServiceRequest/EditService.jsx
+++ src/components/admin/ServiceRequest/EditService.jsx
@@ -1,26 +1,27 @@
 import React, { useState } from "react";
 import Sidenav from "../Sidenav";
+import { useSelector } from "react-redux";
 import axios from "axios";
 import { useHistory } from "react-router-dom";
 
-const EditService = ({ match }) => {
-  console.log(match.params.id);
-  const [title, setTitle] = useState("");
-  const [client, setClient] = useState("");
-  const [manager, setManager] = useState("");
-  const [closing, setClosing] = useState("");
-  const [revenue, setRevenue] = useState("");
-  const [prob, setProb] = useState("");
-  const [priority, setPriority] = useState("");
-  const [status, setStatus] = useState("");
-
+const EditService = ({ id }) => {
+  const results = useSelector((state) => state.service);
+  const services = results.filter((result) => result._id === id);
+  const [title, setTitle] = useState(services.title);
+  const [client, setClient] = useState(services.client);
+  const [manager, setManager] = useState(services.manager);
+  const [closing, setClosing] = useState(services.closing);
+  const [revenue, setRevenue] = useState(services.revenue);
+  const [prob, setProb] = useState(services.probability);
+  const [priority, setPriority] = useState(services.priority);
+  const [status, setStatus] = useState(services.status);
   const history = useHistory();
 
-  const editServiceRequest = (e) => {
+  const editServiceRequest = () => {
     console.log("put");
-    e.preventDefault();
+
     const response = {
-      _id: match.params.id,
+      _id: id,
       title: title,
       client: client,
       manager: manager,
@@ -32,7 +33,7 @@ const EditService = ({ match }) => {
     };
     axios
       .put(
-        `https://crm-backend-nodejs.herokuapp.com/api/admindashboard/servicerequest/${match.params.id}`,
+        `https://crm-backend-nodejs.herokuapp.com/api/admindashboard/servicerequest/${id}`,
         response
       )
       .then((res) => console.log(res.data));
@@ -40,70 +41,135 @@ const EditService = ({ match }) => {
   };
   return (
     <React.Fragment>
-      <div className="grid">
-        <div className="navbar-container">
+      <div className="dashboard">
+        <div className="sidebar">
           <Sidenav />
         </div>
-        <div className="card-container">
-          <div className="add-form">
-            <input
-              type="text"
-              name="title"
-              placeholder="title"
-              onChange={(e) => setTitle(e.target.value)}
-            />
-            <input
-              type="text"
-              name="client"
-              placeholder="client"
-              onChange={(e) => setClient(e.target.value)}
-            />
-            <input
-              type="text"
-              name="manager"
-              placeholder="manager"
-              onChange={(e) => setManager(e.target.value)}
-            />
-            <input
-              type="date"
-              name="closing"
-              placeholder="closing"
-              onChange={(e) => setClosing(e.target.value)}
-            />
-            <input
-              type="text"
-              name="priority"
-              placeholder="priority"
-              onChange={(e) => setPriority(e.target.value)}
-            />
-            <input
-              type="text"
-              name="status"
-              placeholder="status"
-              onChange={(e) => setStatus(e.target.value)}
-            />
-            <input
-              type="text"
-              name="probability"
-              placeholder="probability"
-              onChange={(e) => setProb(e.target.value)}
-            />
-            <input
-              type="text"
-              name="revenue"
-              placeholder="revenue"
-              onChange={(e) => setRevenue(e.target.value)}
-            />
-            <button onClick={(e) => editServiceRequest(e)}>
-              Edit Service Request
-            </button>
-            <button
-              onClick={() => {
-                history.goBack();
-              }}
-            >
-              Back
-            </button>
+        <div className="main-content">
+          <div className="header">
+            <div className="title">Contact</div>
+          </div>
+          <hr />
+          <div className="content">
+            {services.map((result) => (
+              <div key={result._id} className="cards">
+                <ul>
+                  <li>
+                    <b>Title:</b>
+                    <input
+                      type="text"
+                      name="title"
+                      placeholder="title"
+                      value={title}
+                      onChange={(e) => setTitle(e.target.value)}
+                    />
+                  </li>
+                  <li>
+                    <b>Client</b>
+                    <input
+                      type="text"
+                      name="client"
+                      placeholder="client"
+                      value={client}
+                      onChange={(e) => setClient(e.target.value)}
+                    />
+                  </li>
+                  <li>
+                    <b>Manager</b>
+                    <input
+                      type="text"
+                      name="manager"
+                      placeholder="manager"
+                      value={manager}
+                      onChange={(e) => setManager(e.target.value)}
+                    />
+                  </li>
+                  <li>
+                    <b>Expected Closing</b>
+                    <input
+                      type="date"
+                      name="closing"
+                      placeholder="closing"
+                      value={closing}
+                      onChange={(e) => setClosing(e.target.value)}
+                    />
+                  </li>
+                  <li>
+                    <b>Priority</b>
+                    <select name="priority" id="priority">
+                      <option onSelect={() => setPriority("High")}>High</option>
+                      <option onSelect={() => setPriority("Medium")}>
+                        Medium
+                      </option>
+                      <option onSelect={() => setPriority("Low")}>Low</option>
+                    </select>
+                  </li>
+                  <li>
+                    <b>Status</b>
+                    <select name="status" id="status">
+                      <option onSelect={() => setStatus("Created")}>
+                        Created
+                      </option>
+                      <option onSelect={() => setStatus("Released")}>
+                        Released
+                      </option>
+                      <option onSelect={() => setStatus("Open")}>Open</option>
+                      <option onSelect={() => setStatus("In process")}>
+                        In process
+                      </option>
+                      <option onSelect={() => setStatus("Cancelled")}>
+                        Cancelled
+                      </option>
+                      <option onSelect={() => setStatus("Completed")}>
+                        Completed
+                      </option>
+                    </select>
+                  </li>
+                  <li>
+                    <b>Expected Revenue</b>
+                    <input
+                      type="text"
+                      name="revenue"
+                      placeholder="revenue"
+                      value={revenue}
+                      onChange={(e) => setRevenue(e.target.value)}
+                    />
+                  </li>
+                  <li>
+                    <b>Probability</b>
+                    <input
+                      type="text"
+                      name="probability"
+                      placeholder="probability"
+                      value={prob}
+                      defaultValue="0.4"
+                      onChange={(e) => setProb(e.target.value)}
+                    />
+                  </li>
+                </ul>
+                <div className="button-container">
+                  <button
+                    type="button"
+                    onClick={(event) => {
+                      event.preventDefault();
+                      editServiceRequest(result._id);
+                    }}
+                  >
+                    Update
+                    <i className="material-icons">&#xe872;</i>
+                  </button>
+                  <button
+                    type="button"
+                    onClick={() =>
+                      history.push("/admindashboard/servicerequest")
+                    }
+                  >
+                    Back
+                    <i className="material-icons">&#xe3c9;</i>
+                  </button>
+                </div>
+              </div>
+            ))}
           </div>
         </div>
       </div>
--- src/components/admin/ServiceRequest/Service.jsx
+++ src/components/admin/ServiceRequest/Service.jsx
@@ -3,11 +3,14 @@ import { useSelector, useDispatch } from "react-redux";
 import { DelService } from "../../actions/index";
 import { Link } from "react-router-dom";
 import Sidenav from "../Sidenav";
+import { useState } from "react";
+import EditService from "./EditService";
 
 const ServiceRequest = ({ match }) => {
   console.log(match.params.id);
   const results = useSelector((state) => state.service);
   const services = results.filter((result) => result._id === match.params.id);
+  const [view, setView] = useState("noedit");
   const dispatch = useDispatch();
   const url =
     "https://crm-backend-nodejs.herokuapp.com/api/admindashboard/servicerequest";
@@ -39,79 +42,80 @@ const ServiceRequest = ({ match }) => {
   };
   return (
     <React.Fragment>
-      <div className="dashboard">
-        <div className="sidebar">
-          <Sidenav />
-        </div>
-        <div className="main-content">
-          <div className="header">
-            <div className="title">Service Request</div>
-            <Link to="/admindashboard/servicerequest/add">
-              <button type="button">
-                Add <i className="material-icons">&#xe147;</i>
-              </button>
-            </Link>
+      {view === "noedit" && (
+        <div className="dashboard">
+          <div className="sidebar">
+            <Sidenav />
           </div>
-          <hr />
-          <div className="content">
-            {services.map((result) => (
-              <div key={result._id} className="cards">
-                <ul>
-                  <li>
-                    <b>Title:</b>
-                    <p>{result.title}</p>
-                  </li>
-                  <li>
-                    <b>Client</b>
-                    <p>{result.client}</p>
-                  </li>
-                  <li>
-                    <b>Manager</b>
-                    <p>{result.manager}</p>
-                  </li>
-                  <li>
-                    <b>Expected Closing</b>
-                    <p>{result.expected_closing}</p>
-                  </li>
-                  <li>
-                    <b>Priority</b>
-                    <p>{result.priority}</p>
-                  </li>
-                  <li>
-                    <b>Status</b>
-                    <p>{result.status}</p>
-                  </li>
-                  <li>
-                    <b>Expected Revenue</b>
-                    <p>{result.expected_revenue}</p>
-                  </li>
-                  <li>
-                    <b>Probability</b>
-                    <p>{result.probability}</p>
-                  </li>
-                </ul>
-                <div className="button-container">
-                  <Link to={`/admindashboard/servicerequest/${result._id}`}>
-                    <button type="button">
+          <div className="main-content">
+            <div className="header">
+              <div className="title">Service Request</div>
+              <Link to="/admindashboard/servicerequest/add">
+                <button type="button">
+                  Add <i className="material-icons">&#xe147;</i>
+                </button>
+              </Link>
+            </div>
+            <hr />
+            <div className="content">
+              {services.map((result) => (
+                <div key={result._id} className="cards">
+                  <ul>
+                    <li>
+                      <b>Title:</b>
+                      <p>{result.title}</p>
+                    </li>
+                    <li>
+                      <b>Client</b>
+                      <p>{result.client}</p>
+                    </li>
+                    <li>
+                      <b>Manager</b>
+                      <p>{result.manager}</p>
+                    </li>
+                    <li>
+                      <b>Expected Closing</b>
+                      <p>{result.expected_closing}</p>
+                    </li>
+                    <li>
+                      <b>Priority</b>
+                      <p>{result.priority}</p>
+                    </li>
+                    <li>
+                      <b>Status</b>
+                      <p>{result.status}</p>
+                    </li>
+                    <li>
+                      <b>Expected Revenue</b>
+                      <p>{result.expected_revenue}</p>
+                    </li>
+                    <li>
+                      <b>Probability</b>
+                      <p>{result.probability}</p>
+                    </li>
+                  </ul>
+                  <div className="button-container">
+                    <button type="button" onClick={() => setView("edit")}>
                       Update
                       <i className="material-icons">&#xe3c9;</i>
                     </button>
-                  </Link>
-                  <Link
-                    onClick={() => delServiceRequest(result._id)}
-                    to="/admindashboard/servicerequest"
-                  >
-                    <button type="button">
-                      Delete
-                      <i className="material-icons">&#xe872;</i>
-                    </button>
-                  </Link>
+                    <Link
+                      onClick={() => delServiceRequest(result._id)}
+                      to="/admindashboard/servicerequest"
+                    >
+                      <button type="button">
+                        Delete
+                        <i className="material-icons">&#xe872;</i>
+                      </button>
+                    </Link>
+                  </div>
                 </div>
-              </div>
-            ))}
+              ))}
+            </div>
           </div>
         </div>
-      </div>
+      )}
+      {view === "edit" && <EditService id={match.params.id} />}
     </React.Fragment>
   );
 };
