09c4aed4c9706d99dc354d091c1d9429d7fdb19a
shelcia <shelcia1999@gmail.com>
2023-03-24 02:56:15 +0530

    🚀 deploy fix

--- src/index.jsx
+++ src/index.jsx
@@ -2,16 +2,13 @@ import React from "react";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 import { createRoot } from "react-dom/client";
-import { ThemeProvider } from "./context/themeContext";
 
 const container = document.getElementById("root");
 
 const root = createRoot(container);
 
 root.render(
-  <ThemeProvider>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </ThemeProvider>
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>
 );
