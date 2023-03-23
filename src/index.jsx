56c218d26700128b8426338885cb591c11973e9c
shelcia <shelcia1999@gmail.com>
2023-03-24 03:09:08 +0530

    🐛 bug fix

--- src/index.jsx
+++ src/index.jsx
@@ -2,13 +2,16 @@ import React from "react";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 import { createRoot } from "react-dom/client";
+import { ThemeProvider } from "./context/ThemeContext";
 
 const container = document.getElementById("root");
 
 const root = createRoot(container);
 
 root.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
+  <ThemeProvider>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </ThemeProvider>
 );
